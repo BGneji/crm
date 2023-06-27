508e115358f9e17a77b1ef9f629f50014be4b7a5
NEJI <mrkochetov@yandex.ru>
2023-06-27 11:18:43 +0300

    Register_user

--- webcrm/views.py
+++ webcrm/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth import authenticate, login, logout
 from django.contrib import messages
-
+from .forms import SignUpForm
 
 def home(request):
     if request.method == 'POST':
@@ -24,5 +24,26 @@ def logout_user(request):
     logout(request)
     messages.success(request, 'Вы разлогинились ')
     return redirect('home')
+
+
+def register_user(request):
+    form = SignUpForm()
+    if request.method == 'POST':
+        form = SignUpForm(request.POST)
+        if form.is_valid():
+            form.save()
+            username = form.cleaned_data['username']
+            password = form.cleaned_data['password1']
+            # если форма валидна то с разу его логинем
+            user = authenticate(username=username, password=password)
+            login(request, user)
+            # выводим сообщение о успешной регестрации
+            messages.success(request, "Вы залогинились")
+            # перенаправляем на домашнею страницу
+            return redirect('home')
+        else:
+            messages.error(request, 'Ошибка регистрации')
+
+    return render(request, 'register.html', {'form': form})
 
 
